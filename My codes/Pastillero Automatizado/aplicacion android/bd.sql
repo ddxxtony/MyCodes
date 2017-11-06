c233bc754e94457463098c5da9ea0822a5daf410
ddxxtony <elluis.com@live.com>
2017-11-05 21:12:56 -0600

    Update bd.sql

--- My codes/Pastillero Automatizado/aplicacion android/bd.sql
+++ My codes/Pastillero Automatizado/aplicacion android/bd.sql
@@ -1,1 +1,149 @@
 
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: android
+-- ------------------------------------------------------
+-- Server version	5.7.15-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `log`
+--
+
+DROP TABLE IF EXISTS `log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `log` (
+  `idlog` int(11) NOT NULL AUTO_INCREMENT,
+  `fechahoradtoma` datetime DEFAULT NULL,
+  `medicamento_idmedicamento` int(11) NOT NULL,
+  `medicamento_paciente_idpaciente` int(11) NOT NULL,
+  PRIMARY KEY (`idlog`,`medicamento_idmedicamento`,`medicamento_paciente_idpaciente`),
+  KEY `fk_log_medicamento1_idx` (`medicamento_idmedicamento`,`medicamento_paciente_idpaciente`),
+  CONSTRAINT `fk_log_medicamento1` FOREIGN KEY (`medicamento_idmedicamento`, `medicamento_paciente_idpaciente`) REFERENCES `medicamento` (`idmedicamento`, `paciente_idpaciente`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `log`
+--
+
+LOCK TABLES `log` WRITE;
+/*!40000 ALTER TABLE `log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `log` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `medicamento`
+--
+
+DROP TABLE IF EXISTS `medicamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `medicamento` (
+  `idmedicamento` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `dosis` int(11) DEFAULT NULL,
+  `colordg` varchar(45) DEFAULT NULL,
+  `intervalo` bigint(20) DEFAULT NULL,
+  `duraciontx` datetime DEFAULT NULL,
+  `fechayhorado` datetime DEFAULT NULL,
+  `i1` bigint(20) DEFAULT NULL,
+  `i2` bigint(20) DEFAULT NULL,
+  `paciente_idpaciente` int(11) NOT NULL,
+  `s1` varchar(45) NOT NULL DEFAULT '',
+  `s2` varchar(45) NOT NULL DEFAULT '',
+  PRIMARY KEY (`idmedicamento`,`paciente_idpaciente`),
+  KEY `fk_medicamento_paciente1_idx` (`paciente_idpaciente`),
+  CONSTRAINT `fk_medicamento_paciente1` FOREIGN KEY (`paciente_idpaciente`) REFERENCES `paciente` (`idpaciente`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medicamento`
+--
+
+LOCK TABLES `medicamento` WRITE;
+/*!40000 ALTER TABLE `medicamento` DISABLE KEYS */;
+INSERT INTO `medicamento` VALUES (26,'Paracetamol',2,'Rojo',121,'2017-09-28 00:00:00','2017-09-23 15:09:00',2,1,1,'Hora(s)','Minuto(s)'),(28,'Loratadina',1,'Amarillo',80,'2017-09-24 00:00:00','2017-09-24 18:18:00',1,20,1,'Hora(s)','Minuto(s)');
+/*!40000 ALTER TABLE `medicamento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `paciente`
+--
+
+DROP TABLE IF EXISTS `paciente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `paciente` (
+  `idpaciente` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `calle` varchar(45) DEFAULT NULL,
+  `numero` varchar(45) DEFAULT NULL,
+  `colonia` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idpaciente`),
+  UNIQUE KEY `idpaciente_UNIQUE` (`idpaciente`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `paciente`
+--
+
+LOCK TABLES `paciente` WRITE;
+/*!40000 ALTER TABLE `paciente` DISABLE KEYS */;
+INSERT INTO `paciente` VALUES (1,'Carlos','MAtias','234','Soli');
+/*!40000 ALTER TABLE `paciente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `responsable`
+--
+
+DROP TABLE IF EXISTS `responsable`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `responsable` (
+  `idresponsable` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `correo` varchar(45) DEFAULT NULL,
+  `paciente_idpaciente` int(11) NOT NULL,
+  PRIMARY KEY (`idresponsable`,`paciente_idpaciente`),
+  UNIQUE KEY `idresponsable_UNIQUE` (`idresponsable`),
+  UNIQUE KEY `correo_UNIQUE` (`correo`),
+  KEY `fk_responsable_paciente1_idx` (`paciente_idpaciente`),
+  CONSTRAINT `fk_responsable_paciente1` FOREIGN KEY (`paciente_idpaciente`) REFERENCES `paciente` (`idpaciente`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `responsable`
+--
+
+LOCK TABLES `responsable` WRITE;
+/*!40000 ALTER TABLE `responsable` DISABLE KEYS */;
+INSERT INTO `responsable` VALUES (3,'Fernanda','elluis.com@live.com',1);
+/*!40000 ALTER TABLE `responsable` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-05 21:12:07
